8f2d2e04f8e974d8701453f7c7f2e95f92657c5a
Ulug'bek Rustamov <ulugbekrustamov@172-1-2-196.lightspeed.hstntx.sbcglobal.net>
2021-02-17 13:51:18 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,711 @@
+const yolBelgilari = [{
+    id: 0,
+    savol: "Yo'l bering",
+    rasm: "./images/signs/yol_bering.gif"
+  },
+  {
+    id: 1,
+    savol: "Turar-joy dahasi",
+    rasm: "./images/signs/aholi_punkti.gif"
+  },
+  {
+    id: 2,
+    savol: "Asosiy yolning oxiri",
+    rasm: "./images/signs/asosiy_tugash.gif"
+  },
+  {
+    id: 3,
+    savol: "Asosiy yo'lning yo'nalishi",
+    rasm: "./images/signs/asosiy_yol_sxemasi.gif"
+  },
+  {
+    id: 4,
+    savol: "Asosiy yol",
+    rasm: "./images/signs/asosiy_yol.gif"
+  },
+  {
+    id: 5,
+    savol: "Avtomagistral",
+    rasm: "./images/signs/avtomagistral.gif"
+  },
+  {
+    id: 6,
+    savol: "Avtomagistral oxiri",
+    rasm: "./images/signs/avtomagistral_tugashi.gif"
+  },
+  {
+    id: 7,
+    savol: "Velosiped yolkasi",
+    rasm: "./images/signs/velosiped_yolkasi.gif"
+  },
+  {
+    id: 8,
+    savol: "Transport vositalarini yuvish joyi",
+    rasm: "./images/signs/avtomoyka.gif"
+  },
+  {
+    id: 9,
+    savol: "Texnik xizmat korsatish joyi",
+    rasm: "./images/signs/avtoservis.gif"
+  },
+  {
+    id: 10,
+    savol: "Aylanma harakatlanish",
+    rasm: "./images/signs/aylanma_yol.gif"
+  },
+  {
+    id: 11,
+    savol: "Belgilangan yonalishli transport vositalari uchun mo'ljallangan bo'lak",
+    rasm: "./images/signs/belgilangan_yonalishli_transport_yolak.gif"
+  },
+  {
+    id: 12,
+    savol: "Yonilg'i shaxobchasi",
+    rasm: "./images/signs/benzin.gif"
+  },
+  {
+    id: 13,
+    savol: "Bir izli temir yo'l",
+    rasm: "./images/signs/bir_izli_temir.gif"
+  },
+  {
+    id: 14,
+    savol: "Bolalar",
+    rasm: "./images/signs/bolalar.gif"
+  },
+  {
+    id: 15,
+    savol: "Oxiri berk yo'l, ko'cha",
+    rasm: "./images/signs/boshi_berk_kocha.gif"
+  },
+  {
+    id: 16,
+    savol: "Boshqa xavf-xatar",
+    rasm: "./images/signs/boshqa_xavf.gif"
+  },
+  {
+    id: 17,
+    savol: "Chapga burilish taqiqlanadi",
+    rasm: "./images/signs/burilish_taqiqlanadi.gif"
+  },
+  {
+    id: 18,
+    savol: "Eng kam tezlik belgilangan yo'lning oxiri",
+    rasm: "./images/signs/eng_kam_tezlik_oxiri.gif"
+  },
+  {
+    id: 19,
+    savol: "Avtomobillarni ko'rikdan o'tkazish joyi",
+    rasm: "./images/signs/estakada.gif"
+  },
+  {
+    id: 20,
+    savol: "Harakatlanish taqiqlangan",
+    rasm: "./images/signs/faqat_odamlaga.gif"
+  },
+  {
+    id: 21,
+    savol: "Harakatlanish chapga",
+    rasm: "./images/signs/harakatlanish_chapga.gif"
+  },
+  {
+    id: 22,
+    savol: "Harakatlanish o'nga",
+    rasm: "./images/signs/harakatlanish_onga.gif"
+  },
+  {
+    id: 23,
+    savol: "Harakatlanish o'nga yoki tog'riga",
+    rasm: "./images/signs/harakatlanish_onga_togriga.gif"
+  },
+  {
+    id: 24,
+    savol: "Harakatlanish tasviri",
+    rasm: "./images/signs/harakatlanish_tasviri.gif"
+  },
+  {
+    id: 25,
+    savol: "Harakatlanish tog'riga",
+    rasm: "./images/signs/harakatlanish_togriga.gif"
+  },
+  {
+    id: 26,
+    savol: "Ichimlik suvi",
+    rasm: "./images/signs/ichimlik_suvi.gif"
+  },
+  {
+    id: 27,
+    savol: "Ikki tomonlama harakatlanish",
+    rasm: "./images/signs/ikki_tomonlama_xarakatlanish.gif"
+  },
+  {
+    id: 28,
+    savol: "Dam olish joyi",
+    rasm: "./images/signs/kemping.gif"
+  },
+  {
+    id: 29,
+    savol: "Kirish taqiqlangan",
+    rasm: "./images/signs/kirish_taqiqlanadi.gif"
+  },
+  {
+    id: 30,
+    savol: "Mehmonxona",
+    rasm: "./images/signs/kasalxona.gif"
+  },
+  {
+    id: 31,
+    savol: "Kotarma koprik",
+    rasm: "./images/signs/kotarma_koprik.gif"
+  },
+  {
+    id: 32,
+    savol: "Cheklangan balandlik",
+    rasm: "./images/signs/maksimal_balandlik.gif"
+  },
+  {
+    id: 33,
+    savol: "Cheklangan kenglik",
+    rasm: "./images/signs/maksimal_kenglik.gif"
+  },
+  {
+    id: 34,
+    savol: "Vazn cheklangan",
+    rasm: "./images/signs/maksimal_ogrlik.gif"
+  },
+  {
+    id: 35,
+    savol: "Cheklangan uzunlik",
+    rasm: "./images/signs/maksimal_uzunlik.gif"
+  },
+  {
+    id: 36,
+    savol: "Avtobus va trolleybus to'xtash joyi",
+    rasm: "./images/signs/marshrut.gif"
+  },
+  {
+    id: 37,
+    savol: "Mexanik transport vositalarining harakatlanishi taqiqlangan",
+    rasm: "./images/signs/mexanik_taqiqlangan.gif"
+  },
+  {
+    id: 38,
+    savol: "Mototsikllar harakatlanishi taqiqlangan",
+    rasm: "./images/signs/motosikl_taqiqlanadi.gif"
+  },
+  {
+    id: 39,
+    savol: "Eng kam oraliq",
+    rasm: "./images/signs/oraliq_masofa.gif"
+  },
+  {
+    id: 40,
+    savol: "Oshxona",
+    rasm: "./images/signs/oshxona.gif"
+  },
+  {
+    id: 41,
+    savol: "Ot-arava harakatlanishi taqiqlangan",
+    rasm: "./images/signs/ot_arava_taqiqlanadi.gif"
+  },
+  {
+    id: 42,
+    savol: "To'xtab turish joyi",
+    rasm: "./images/signs/parkovka.gif"
+  },
+  {
+    id: 43,
+    savol: "Pastlab uchuvchi samolyotlar",
+    rasm: "./images/signs/pastlab_uchuvchi_samolyot.gif"
+  },
+  {
+    id: 44,
+    savol: "Piyodalar o'tish joyi",
+    rasm: "./images/signs/piyodalar_otish_joyi.gif"
+  },
+  {
+    id: 45,
+    savol: "Piyodalar yo'lkasi",
+    rasm: "./images/signs/piyodalar_yolagi.gif"
+  },
+  {
+    id: 46,
+    savol: "Shlagbaumsiz temir yo'l kesishmasi",
+    rasm: "./images/signs/poyezd.gif"
+  },
+  {
+    id: 47,
+    savol: "Qayrilish ta'qiqlanadi",
+    rasm: "./images/signs/qayrilish_taqiqlanadi.gif"
+  },
+  {
+    id: 48,
+    savol: "Boshqa qatnov qismiga qayta tizilishning boshlang'ich ko'rsatkichi",
+    rasm: "./images/signs/qayta_tizilish_sxemasi.gif"
+  },
+  {
+    id: 49,
+    savol: "Quvib o'tish taqiqlanadi",
+    rasm: "./images/signs/quvib_otish_taqiqlanadi.gif"
+  },
+  {
+    id: 50,
+    savol: "Quvib o'tish ta'qiqlangan hududning oxiri",
+    rasm: "./images/signs/quvib_otish_taqiqlanadi_tugadi.gif"
+  },
+  {
+    id: 51,
+    savol: "Radar",
+    rasm: "./images/signs/radar.jpg"
+  },
+  {
+    id: 52,
+    savol: "Ro'paradagi harakatlanishga nisbatan imtiyoz",
+    rasm: "./images/signs/ropara_imtiyo.gif"
+  },
+  {
+    id: 53,
+    savol: "Ro'para harakatlanishning ustunligi",
+    rasm: "./images/signs/ropara_ustunlik.gif"
+  },
+  {
+    id: 54,
+    savol: "Shlagbaumli temir yo'l kesishmasi",
+    rasm: "./images/signs/shlakbaumli_temiryol.gif"
+  },
+  {
+    id: 55,
+    savol: "Sirpanchiq yo'l",
+    rasm: "./images/signs/sirpanchiq_yol.gif"
+  },
+  {
+    id: 56,
+    savol: "Sohilga chiqish",
+    rasm: "./images/signs/soxilga_chiqish.gif"
+  },
+  {
+    id: 57,
+    savol: "To'xtamasdan harakatlanish taqiqlangan",
+    rasm: "./images/signs/stop.gif"
+  },
+  {
+    id: 58,
+    savol: "Svetofor tartibga soladi",
+    rasm: "./images/signs/svetofor.gif"
+  },
+  {
+    id: 59,
+    savol: "Ta'mirlash ishlari",
+    rasm: "./images/signs/tamirlash_ishlari.gif"
+  },
+  {
+    id: 60,
+    savol: "Tasir oralig'i",
+    rasm: "./images/signs/tasir_doirasi.gif"
+  },
+  {
+    id: 61,
+    savol: "Taksi to'xtab turish joyi",
+    rasm: "./images/signs/taxi.gif"
+  },
+  {
+    id: 62,
+    savol: "Telefon",
+    rasm: "./images/signs/telefon.gif"
+  },
+  {
+    id: 63,
+    savol: "Temir yo'l kesishmasining yaqinligi haqida ogohlantirish",
+    rasm: "./images/signs/temiryol_kesishmasining_yaqinligi.gif"
+  },
+  {
+    id: 64,
+    savol: "Yuqori tezlik cheklangan",
+    rasm: "./images/signs/tezlik-cheklangan.gif"
+  },
+  {
+    id: 65,
+    savol: "Tik nishablik",
+    rasm: "./images/signs/tik_nishablik.gif"
+  },
+  {
+    id: 66,
+    savol: "Tik balandlik",
+    rasm: "./images/signs/tik_qiyalik.gif"
+  },
+  {
+    id: 67,
+    savol: "Tirkama bilan harakatlanish taqiqlangan",
+    rasm: "./images/signs/tirkama_taqiq.gif"
+  },
+  {
+    id: 68,
+    savol: "Xojatxona",
+    rasm: "./images/signs/toilet.gif"
+  },
+  {
+    id: 69,
+    savol: "Tonnel",
+    rasm: "./images/signs/tonnel.gif"
+  },
+  {
+    id: 70,
+    savol: "Tosh otilishi",
+    rasm: "./images/signs/tosh_otilishi.gif"
+  },
+  {
+    id: 71,
+    savol: "Toshlar tushishi",
+    rasm: "./images/signs/tosh_tushishi.gif"
+  },
+  {
+    id: 72,
+    savol: "To'siqni o'ngdan yoki chapdan chetlab o'tish",
+    rasm: "./images/signs/tosiq_chap_ili_ong.gif"
+  },
+  {
+    id: 73,
+    savol: "To'siqni chapdan chetlab o'tish",
+    rasm: "./images/signs/tosiq_chapdan.gif"
+  },
+  {
+    id: 74,
+    savol: "To'siqni chapdan o'ngdan chetlab o'tish",
+    rasm: "./images/signs/tosiq_ongdan.gif"
+  },
+  {
+    id: 75,
+    savol: "Tovush moslamalaridan foydalanish ta'qiqlangan",
+    rasm: "./images/signs/tovushli_signal.gif"
+  },
+  {
+    id: 76,
+    savol: "To'xtab turish ta'qiqlangan",
+    rasm: "./images/signs/toxtab_turish.gif"
+  },
+  {
+    id: 77,
+    savol: "To'xtash taqiqlangan",
+    rasm: "./images/signs/toxtash_taqiqlanadi.gif"
+  },
+  {
+    id: 78,
+    savol: "Transport vositasini to'xtab turish joyiga qo'yish usuli",
+    rasm: "./images/signs/toxtash_usuli.gif"
+  },
+  {
+    id: 79,
+    savol: "Traktorlar harakatlanishi taqiqlangan",
+    rasm: "./images/signs/traktor.gif"
+  },
+  {
+    id: 80,
+    savol: "Tramvay yo'li bilan kesishuv",
+    rasm: "./images/signs/tramvay_kesishuv.gif"
+  },
+  {
+    id: 81,
+    savol: "Tramvay to'xtash joyi",
+    rasm: "./images/signs/tramvay.gif"
+  },
+  {
+    id: 82,
+    savol: "Transport vositasining turi",
+    rasm: "./images/signs/transport_vositasi_turi.gif"
+  },
+  {
+    id: 83,
+    savol: "Turar-joy dahasining oxiri",
+    rasm: "./images/signs/turar_joy_tugash.gif"
+  },
+  {
+    id: 84,
+    savol: "Mol haydab o'tish",
+    rasm: "./images/signs/uy_hayvonlari.gif"
+  },
+  {
+    id: 85,
+    savol: "Velosipedda harakatlanish ta'qiqlangan",
+    rasm: "./images/signs/velik_taqiqlanadi.gif"
+  },
+  {
+    id: 86,
+    savol: "Velosiped yo'lkasi bilan kesishuv",
+    rasm: "./images/signs/velosiped.gif"
+  },
+  {
+    id: 87,
+    savol: "Piyodalarning yer ostidan o'tish joyi",
+    rasm: "./images/signs/yer_osti_yoli.gif"
+  },
+  {
+    id: 88,
+    savol: "Piyodalarning yer ustidan o'tish joyi",
+    rasm: "./images/signs/yer_usti_yoli.gif"
+  },
+  {
+    id: 89,
+    savol: "Yonlama shamol",
+    rasm: "./images/signs/yonlama_shamol.gif"
+  },
+  {
+    id: 90,
+    savol: "Yovvoyi hayvonlar",
+    rasm: "./images/signs/yovvoyi_hayvonlar.gif"
+  },
+  {
+    id: 91,
+    savol: "Yuk avtomobillarida quvib o'tish taqiqlangan",
+    rasm: "./images/signs/yuk_avtomobil_quvish_taqiqlanadi.gif"
+  },
+  {
+    id: 92,
+    savol: "Yuk avtommobillarida quvib o'tish taqiqlangan hududning oxiri",
+    rasm: "./images/signs/yuk_avtomobil_quvish_taqiqlanadi_oxiri.gif"
+  },
+  {
+    id: 93,
+    savol: "Tirbandlik",
+    rasm: "./images/signs/tirbandlik.gif"
+  },
+  {
+    id: 94,
+    savol: "Notekis yo'l",
+    rasm: "./images/signs/notekis_yol.gif"
+  },
+  {
+    id: 95,
+    savol: "Barcha cheklovlarning oxiri",
+    rasm: "./images/signs/barcha.gif"
+  },
+  {
+    id: 96,
+    savol: "Tavsiya etilgan tezlik",
+    rasm: "./images/signs/tavsiya_etilgan_tezlik.gif"
+  },
+  {
+    id: 97,
+    savol: "Surat va video",
+    rasm: "./images/signs/surat_foto.png"
+  },
+  {
+    id: 98,
+    savol: "Falokatli holatlar uchun kirish yo'li",
+    rasm: "./images/signs/xatar_yol.gif"
+  },
+  {
+    id: 99,
+    savol: "Shifoxona",
+    rasm: "./images/signs/shifoxona.gif"
+  },
+  {
+    id: 100,
+    savol: "Yuqori tezlik cheklangan hududning oxiri",
+    rasm: "./images/signs/maksimal_tezlik_tugadi.gif"
+  },
+];
+
+let btn = document.querySelector('.btn');
+let daraja = document.querySelector('.daraja');
+
+let timescs = document.querySelector('.vaqt');
+let game = document.querySelector('.game');
+
+let savollar = document.querySelector('.savollar-text');
+let blockImg = document.querySelector('.block-img');
+
+let savollarBlockInner = document.querySelector('.savollar-block-inner')
+let timeSekond = document.querySelector('.time')
+
+let errorMusic = document.querySelector('.error-music')
+let section1 = document.querySelector('.section1');
+
+let oyndavomi = document.querySelector('.oyndavomi');
+let gameOver = document.querySelector('.end');
+
+let torijovob = document.querySelector('.torijovob');
+let winLink = document.querySelector('.win-link');
+
+let winBack = document.querySelector('.win-back');
+let winBlock = document.querySelector('.win');
+
+let musicOne = document.querySelector('.musicone');
+let blockFirstOne = document.querySelector('.block-first-one');
+
+let pobeda = document.querySelector('.pobeda');
+let winOne = document.querySelector('.win-one')
+
+let savollarBlock = document.querySelector('.savollar-block')
+let savolBlockTop = document.querySelector('.savollar-block-top');
+
+let timerItem = document.querySelector('#timer-item');
+
+winOne.addEventListener('click', function () {
+  window.location.reload();
+})
+
+window.addEventListener('load', function () {
+  musicOne.play();
+});
+
+
+
+
+
+
+
+
+
+
+
+// function timeCalc() {
+//   var timeleft = time.value;
+//   var downloadTimer = setInterval(function () {
+
+//     if (timeleft <= 0) {
+//       clearInterval(downloadTimer);
+//       gameOver.play();
+//       document.getElementById("countdown").innerHTML = timeleft;
+//       winBack.classList.add('active')
+//       winBlock.classList.add('active')
+//     } else {
+//       let strTimer = `${Math.trunc(hour)}:${Math.trunc(minutes)}:${seconds}`;
+//       document.getElementById("countdown").innerHTML = strTimer;
+//     }
+//     timeleft -= 1;
+//   }, 1000);
+// }
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getItems(numOfItems) {
+  for (var i = 0; i < numOfItems; i++) {
+    var idx = Math.floor(Math.random() * yolBelgilari.length);
+    newArr.push(yolBelgilari[idx]);
+    yolBelgilari.splice(idx, 1);
+  }
+}
+
+function questionGenerator(randomQuestionId) {
+  let randomId = getRandomInt(newArr.length);
+  if (randomQuestionId == randomId) {
+    questionGenerator(randomId);
+  } else {
+    savollar.textContent = newArr[randomId].savol;
+    savollar.dataset.itemId = newArr[randomId].id;
+  }
+}
+
+var newArr = [];
+
+
+
+function createElement() {
+  for (index = 0; index < newArr.length; index++) {
+    let savolBlock = document.createElement('a');
+    let imgBlock = document.createElement('img');
+    let randomQuestionId = getRandomInt(newArr.length);
+    savolBlock.setAttribute('href', 'javascript:void(0)')
+    savolBlock.setAttribute('data-button-order', `${newArr[index].id}`)
+    savolBlock.setAttribute('class', 'avto-link');
+    savollarBlockInner.appendChild(savolBlock)
+    savolBlock.appendChild(imgBlock);
+    imgBlock.src = newArr[index].rasm
+    savollar.textContent = newArr[randomQuestionId].savol;
+    savollar.dataset.itemId = newArr[randomQuestionId].id;
+
+
+    savolBlock.addEventListener('click', () => {
+      if (savolBlock.dataset.buttonOrder == savollar.dataset.itemId) {
+        savolBlock.style.background = 'blue'
+        savolBlock.style.visibility = 'hidden';
+        savolBlock.style.transform = 'scale(1.3)'
+        const clickednameIndex = newArr.findIndex(objSavol => objSavol.savol === savollar.textContent)
+        newArr.splice(clickednameIndex, 1);
+        torijovob.play();
+        if (newArr.length == 0) {
+          blockFirstOne.classList.add('active')
+          pobeda.play();
+          // game.pause();
+          
+          timerItem.style.display = 'none'
+          gameOver.pause()
+        }
+        questionGenerator(randomQuestionId);
+      } else {
+        savolBlock.style.background = 'red'
+        errorMusic.play();
+      }
+    })
+  }
+};
+
+
+
+
+
+
+
+
+btn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+
+  
+  savolBlockTop.classList.add('active');
+
+
+  timerItem.classList.add('active');
+  game.classList.add('active');
+
+  if (daraja.value === '20') {
+    getItems(20)
+  }
+  if (daraja.value === '40') {
+    getItems(40)
+  }
+  if (daraja.value === '60') {
+    getItems(60)
+  }
+  createElement();
+
+  timeMinut = parseInt(timescs.value) * 60;
+
+
+
+  timer = setInterval(function () {
+    seconds = timeMinut % 60
+    minutes = timeMinut / 60 % 60
+    hour = timeMinut / 60 / 60 % 60
+
+    if (timeMinut < 32) {
+      oyndavomi.play();
+      timerItem.style.color = 'red'
+    }
+
+    if (timeMinut <= 0) {
+      gameOver.play();
+      winBack.classList.add('active')
+      console.log(winBack)
+      winBlock.classList.add('active')
+      clearInterval(timer);
+    } else {
+
+      let strTimer = `${Math.trunc(hour)}:${Math.trunc(minutes)}:${seconds}`;
+
+      timerItem.innerHTML = strTimer;
+    }
+    --timeMinut;
+  }, 1000)
+});
+
+
+
+
+
+
+winLink.addEventListener('click', function () {
+  window.location.reload();
+});
